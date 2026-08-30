855907d71d47c4189689cc628ff127c84e64ff5f
Jaloldin007 <shahandtel265@gmail.com>
2026-08-30 15:28:10 +0300

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ st.set_page_config(
     page_icon="🐄",
     layout="wide",
     initial_sidebar_state="expanded",
+)
     # ===========================================
 # YANGILANGAN MA'LUMOTLAR (URG'OCHI 87)
 # ===========================================
